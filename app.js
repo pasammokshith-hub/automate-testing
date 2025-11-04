beb9b8403d099ececf77105939a3163f6e3c740d
pasammokshith-hub <pasammokshith@gmail.com>
2025-11-04 23:54:23 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById('changeBtn').addEventListener('click', () => {
+  const message = document.getElementById('message');
+  message.textContent = 'Message changed by JavaScript!';
+});
